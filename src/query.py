da90ec58e1a15a37c89b4e12991e5f9a9da85a69
EthanChen0418 <ethan_chen@DESKTOP-A1BE1FI.localdomain>
2020-07-26 17:34:15 +0800

    query database

---
+++ src/query.py
@@ -0,0 +1,42 @@
+import os
+import json
+
+class dataBase:
+	def __init__(self, dir):
+		self.db = {}
+
+		with open(os.path.join(dir, "attraction_db.json")) as f_att:
+			self.db["attraction"] = json.load(f_att)
+
+		with open(os.path.join(dir, "hospital_db.json")) as f_hos:
+			self.db["hospital"] = json.load(f_hos)
+
+		with open(os.path.join(dir, "hotel_db.json")) as f_hot:
+			self.db["hotel"] = json.load(f_hot)
+
+		with open(os.path.join(dir, "police_db.json")) as f_pol:
+			self.db["police"] = json.load(f_pol)
+
+		with open(os.path.join(dir, "restaurant_db.json")) as f_res:
+			self.db["restaurant"] = json.load(f_res)
+
+		with open(os.path.join(dir, "taxi_db.json")) as f_tax:
+			self.db["taxi"] = json.load(f_tax)
+
+		with open(os.path.join(dir, "train_db.json")) as f_tra:
+			self.db["train"] = json.load(f_tra)
+
+	def query(b, pre_b):
+		results = []
+
+		for dom_key, dom_values in b.items():
+			if dom_values != pre_b[dom_key]:
+				dom_results = self.db[dom_key]
+
+				for state_key, state_values in b[dom_key]:
+					if state_key in self.db[dom_key].keys() and "dontcare" not in state_values:
+						dom_results = [x if x[state_key] in state_values for x in dom_results]
+
+				results += dom_results
+
+		return results
