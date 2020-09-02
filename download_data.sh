be0f5cc41e8b502499dd7a4730a1399c0889aa28
adamlin120 <31605305+adamlin120@users.noreply.github.com>
2020-09-02 15:05:53 +0800

    Update download_data.sh

--- download_data.sh
+++ download_data.sh
@@ -1,2 +1,4 @@
 # download crosswoz
-wget https://ytlin.s3-ap-northeast-1.amazonaws.com/data/crosswoz.zip
+wget https://ytlin.s3-ap-northeast-1.amazonaws.com/data/crosswoz.zip -o crosswoz.zip
+unzip crosswoz.zip
+rm crosswoz.zip
